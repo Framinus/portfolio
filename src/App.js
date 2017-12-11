4a2b1c4920c82b5392dbd5fd0a2988384e5c2ca8
James McCormack <thejamesmccormack@gmail.com>
2017-12-11 10:20:34 -0800

    adding file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hey there, everyone! This is a portfolio!
+      </div>
+    );
+  }
+}
+
+export default App;
