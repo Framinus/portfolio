fb65db0154b80946c2772d266357cacd87200856
James McCormack <thejamesmccormack@gmail.com>
2018-01-15 19:52:27 -0800

    added profile pic

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,14 +5,17 @@ const Profile = function() {
   return (
     <div className="profile-container">
       <div className="profile-pic">
-        <img className="james-pic" src="http://www.sheffield.com/wp-content/uploads/2013/06/placeholder.png" alt="james" />
+        <img className="james-pic" src="./images/james_bowtie.jpg" alt="james" />
       </div>
       <div className="profile-text">
         <h2 className="profile-title">
           Full Stack JavaScript Developer
         </h2>
+        <h4 className="tech-stack">
+          Node.js, Express, React, PostgreSQL, Mocha/Chai, HTML and CSS.
+        </h4>
         <p>
-          Proficient in Node.js, Express, React, PostgreSQL, Mocha/Chai, HTML and CSS. I transitioned from self-guided learning to Learners Guild, a full time ten month program in Oakland, CA that dives into all aspects of full stack web development using JavaScript as the primary language. I have built multiple applications and have a solid understanding of RESTful APIs and Test Driven Development.
+           I transitioned from self-guided learning to Learners Guild, a full time ten month program in Oakland, CA that dives into all aspects of full stack web development using JavaScript as the primary language. I have built multiple applications and have a solid understanding of RESTful APIs and Test Driven Development.
         </p>
         <p>
           I truly believe that learning is a life long process - I learned to ride a bike in my twenties! Experience matters, but so does the willingness to find unusual answers, solve problems, and most of all, possess the grit and humility to struggle and prevail in unfamiliar territory. I love web development because it constantly changes and will continue to change, affording me the opportunity to grow and change with it!
